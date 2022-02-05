96a7785621d62ce7c1e0dee2b763c23e90516b6e
Oksana Orlova <kseniyaorlova88@gmail.com>
2022-02-05 19:18:20 +0300

    Правки дз 11

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import './sass/main.scss';
 import PixabayApi from './pixabay-api';
+import Notiflix from 'notiflix';
 
 
 fetch ('https://pixabay.com/api/?key=11538931-2af4b5ad07badda1fd47b2159&q=cat&image_type=photo&orientation=horizontal&safesearch=true&per_page=40&page=1')
@@ -16,16 +17,30 @@ loadMoreBtn.addEventListener ('click', onLoadMore)
 form.addEventListener ('submit', onSearch);
 
 function onSearch(e) {
-    e.preventDefault();
+  e.preventDefault();
+  clearContainer();
+  pixabayApi.searchQuery = e.currentTarget.elements.searchQuery.value;
+  pixabayApi.resetPage ();
+pixabayApi.fetchPicture().then(hits => {
+  containerMarkup(hits)
+  console.log(pixabayApi.totalHits.totalHits)
+  if (pixabayApi.totalHits.totalHits > 40) {
+    loadMoreBtn.classList.remove('is-hidden')
+  }
 
-    loadMoreBtn.classList.remove ('is-hidden');
+  if (pixabayApi.totalHits.totalHits <= 40 && hits.length !== 0 ) {
+        Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
+                                loadMoreBtn.classList.add ('is-hidden');
+      }
 
-    clearContainer();
-    pixabayApi.searchQuery = e.currentTarget.elements.searchQuery.value;
-    pixabayApi.resetPage ();
-    pixabayApi.fetchPicture().then(containerMarkup);
-    
+      if (hits.length === 0) {
+        Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+                  loadMoreBtn.classList.add ('is-hidden');
+      }
+});
 };
+
+    
 
 function onLoadMore() {
     pixabayApi.fetchPicture().then(containerMarkup);
--- src/pixabay-api.js
+++ src/pixabay-api.js
@@ -25,15 +25,6 @@ export default class PixabayApi {
 
           console.log(response.data.hits)
 
-          if (response.data.hits.length === 0) {
-                    Notiflix.Notify.failure('Sorry, there are no images matching your search query. Please try again.');
-      
-                }
-
-                if (response.data.hits / 40 !== 1 && response.data.hits.length !== 0 ) {
-                            Notiflix.Notify.failure("We're sorry, but you've reached the end of search results.");
-                            
-                        }
 
           return response.data.hits;
 
